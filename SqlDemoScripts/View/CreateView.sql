e65a5dfdc242360ae90175378eea36cfb674dc7c
Julian Dittmann <julian.dittmann@mail.de>
2020-10-07 00:43:02 +0200

    Extension Guid added to tablename (Sandbox v17.0.17126.17319)

--- SqlDemoScripts/View/CreateView.sql
+++ SqlDemoScripts/View/CreateView.sql
@@ -1,1 +1,1 @@
-CREATE VIEW [sampleview] AS SELECT a.No_ , a.Name, a.Address FROM [CRONUS DE$Customer] as a;
+CREATE VIEW [sampleview] AS SELECT a.No_ , a.Name, a.Address FROM [CRONUS DE$Customer$437dbf0e-84ff-417a-965d-ed2bb9650972] as a;
