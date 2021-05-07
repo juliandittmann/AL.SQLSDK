40221f5f822b5402d504d5eb1c46ad64bd4d68ab
Julian Dittmann <julian.dittmann@mail.de>
2021-05-07 18:15:05 +0200

    Sample updated to us container

--- Demo/src/Examples/View/scripts/CreateView.sql
+++ Demo/src/Examples/View/scripts/CreateView.sql
@@ -1,1 +1,1 @@
-CREATE VIEW [sampleview] AS SELECT a.No_ , a.Name, a.Address FROM [CRONUS DE$Customer$437dbf0e-84ff-417a-965d-ed2bb9650972] as a;
+CREATE VIEW [sampleview] AS SELECT a.No_ , a.Name, a.Address FROM [CRONUS USA, Inc_$Customer$437dbf0e-84ff-417a-965d-ed2bb9650972] as a;
